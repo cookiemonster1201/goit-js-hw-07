2212d5ce71b3620516a73c1a77206e48d55d2ff3
Алена Микитюк <alenamikituk@Alena.local>
2021-06-28 12:09:57 +0300

    task 1 done

--- js/task-01.js
+++ js/task-01.js
@@ -1,1 +1,16 @@
-console.dir(document)
+const categoriesListRef = document.querySelector("#categories");
+const subcategoriesListRef = document.querySelectorAll(".item");
+
+const countCategories = (categoriesList) =>
+  console.log(`В списке ${categoriesListRef.childElementCount} категории`);
+countCategories(categoriesListRef);
+
+const showSubcategoryDetails = (subcategoriesList) => {
+  subcategoriesList.forEach((subCategory) => {
+    let titleRef = subCategory.firstElementChild;
+    console.log(
+      `Категория: '${titleRef.textContent}'; Количество элементов: ${titleRef.nextElementSibling.childElementCount}`
+    );
+  });
+};
+showSubcategoryDetails(subcategoriesListRef);
